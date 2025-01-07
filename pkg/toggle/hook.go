c91355480c7e078c02cfc33e77b1dede2076023e
Abraxas-365 <luisfmiranda8@gmail.com>
2025-01-07 07:05:13 +0700

    fix: telemetry

--- pkg/toggle/hook.go
+++ pkg/toggle/hook.go
@@ -52,11 +52,43 @@ func (h *ProviderHook) After(ctx context.Context, hookContext openfeature.HookCo
 	}
 
 	evalCtx := hookContext.EvaluationContext()
+
+	attributes := evalCtx.Attributes()
+
+	flattenedAttributes := make(map[string]interface{})
+
+	if region, ok := attributes["region"].(string); ok {
+		flattenedAttributes["region"] = region
+	}
+	if subscriptionLevel, ok := attributes["subscriptionLevel"].(string); ok {
+		flattenedAttributes["subscriptionLevel"] = subscriptionLevel
+	}
+	if ipAddress, ok := attributes["ipAddress"].(string); ok {
+		flattenedAttributes["ipAddress"] = ipAddress
+	}
+
+	if user, ok := attributes["user"].(map[string]interface{}); ok {
+		if email, ok := user["email"].(string); ok {
+			flattenedAttributes["user.email"] = email
+		}
+		if id, ok := user["id"].(string); ok {
+			flattenedAttributes["user.id"] = id
+		}
+		if name, ok := user["name"].(string); ok {
+			flattenedAttributes["user.name"] = name
+		}
+		if userCustomAttrs, ok := user["customAttributes"].(map[string]interface{}); ok {
+			if role, ok := userCustomAttrs["role"].(string); ok {
+				flattenedAttributes["user.role"] = role
+			}
+		}
+	}
+
 	hyphenCtx := EvaluationContext{
 		TargetingKey:     evalCtx.TargetingKey(),
 		Application:      h.provider.config.Application,
 		Environment:      h.provider.config.Environment,
-		CustomAttributes: evalCtx.Attributes(),
+		CustomAttributes: flattenedAttributes,
 	}
 
 	payload := TelemetryPayload{
